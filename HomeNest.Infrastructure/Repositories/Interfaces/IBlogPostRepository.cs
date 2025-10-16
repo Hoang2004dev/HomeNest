884e7e565f07c5ced540d15f564bb06630e492b9
hoang2004dev <hoang2004dev@gmail.com>
2025-10-16 15:57:59 +0700

    blog post

---
+++ HomeNest.Infrastructure/Repositories/Interfaces/IBlogPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using HomeNest.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeNest.Infrastructure.Repositories.Interfaces
+{
+    public interface IBlogPostRepository : IGenericRepository<BlogPost>
+    {
+
+    }
+}
